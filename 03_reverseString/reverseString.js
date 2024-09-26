a2a0d6b734906b9e2dbfd5fa625fdc371235a0c9
cheesydoritos08 <163181834+cheesydoritos08@users.noreply.github.com>
2024-09-25 20:21:36 -0400

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    console.log(stringArray);
 
+    let reversedString = ""
+    for (i=stringArray.length-1; i>=0; i--){
+        reversedString += stringArray[i];
+        console.log(reversedString);
+    }
+
+    return reversedString;
+    
 };
 
 // Do not edit below this line
