d835756457fef66db56b00b1e6e5e65f9e4d9d7c
cheesydoritos08 <163181834+cheesydoritos08@users.noreply.github.com>
2024-09-27 14:14:29 -0400

    Finish exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementRemoved) {
+    let filteredArray = array;
+    console.log(filteredArray)
+    console.log(elementRemoved)
+    for (const element of elementRemoved){
+        console.log(filteredArray.indexOf(element))
+        filteredArray = filteredArray.filter((word) => word !== element);
+       console.log(filteredArray);
+    }
+
+    return filteredArray;
 };
 
 // Do not edit below this line
