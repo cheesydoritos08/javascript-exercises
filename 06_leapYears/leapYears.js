cc9e4a89444a2f1b79af5810dff546b1f2b741e8
cheesydoritos08 <163181834+cheesydoritos08@users.noreply.github.com>
2024-09-27 15:10:25 -0400

    Finish exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(number) {
+    if (number % 4 == 0){
+        console.log("Divisible by 4");
+        if (number % 100 == 0){
+            console.log("Divisble by 100")
+            if (number % 400 == 0){
+                console.log("Divisible by 400")
+                return true;
+            }
+            return false;
+        } 
 
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
