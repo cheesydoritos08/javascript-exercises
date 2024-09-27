97aabbb16c800f440fa6c78b146b04495cbae5e4
cheesydoritos08 <163181834+cheesydoritos08@users.noreply.github.com>
2024-09-27 14:44:51 -0400

    Finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let bigNum;
+    let littleNum;
+    let sumNum = 0;
+    if (typeof num1 != "number" || typeof num2 != "number"){
+        return "ERROR";
+    }
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    if (num1 != Math.floor(num1) || num2 != Math.floor(num2)){
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        bigNum = num1;
+        littleNum = num2;
+    } else {
+        bigNum = num2;
+        littleNum = num1;
+    }
+    for (let i = littleNum; i <= bigNum; i++){
+        sumNum+=i;
+    } 
+    return sumNum;
 };
 
 // Do not edit below this line
