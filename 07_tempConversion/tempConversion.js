b06ac50cd326352d8f10f856cfed9b38a0e66d60
cheesydoritos08 <163181834+cheesydoritos08@users.noreply.github.com>
2024-09-27 15:25:10 -0400

    Finish exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let degreesCelsius = (degrees-32)*(5/9);
+  return parseFloat(degreesCelsius.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let degreesFahrenheit = (degrees * (9/5)) + 32;
+  return parseFloat(degreesFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
